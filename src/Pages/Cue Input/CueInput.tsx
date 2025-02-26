0c6ada4dcce223c20d9d6d781eeb71a78d684d5e
Kush Patel <kushptl@udel.edu>
2025-02-25 22:07:41 -0500

    Cue Input Prototyping

--- src/Pages/Cue Input/CueInput.tsx
+++ src/Pages/Cue Input/CueInput.tsx
@@ -1,11 +1,120 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import './CueInput.css';
+import { AppHeader } from '../../Components/Header/Header';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import addMoreButton from '../../Assets/Home-Page/Add-More-Button.png';
+import editButton from '../../Assets/Home-Page/Edit-Button.png';
+import liveButton from '../../Assets/Home-Page/Live-Button.png';
+import { Project } from '../../Interfaces/Project/Project';
+import { Cue } from '../../Interfaces/Cue/Cue';
 
 function CueInput() {
+  const navigate = useNavigate();
+
+  const cues: Cue[]=[
+    {cueNumber: 1, title: "Pre-show Seating", startTime: new Date("2024-12-21T17:25:00"), endTime: new Date("2024-12-21T17:40:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Logo Splash",
+      centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Purple", notes: "Blackout, curtains closed, slow gobo movements, haze, audio is MM Jingle"}, 
+      {cueNumber: 2, title: "Countdown", startTime: new Date("2024-12-21T17:40:00"), endTime: new Date("2024-12-21T17:45:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
+        centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"}, 
+        {cueNumber: 3, title: "Bang Video", startTime: new Date("2024-12-21T17:45:00"), endTime: new Date("2024-12-21T17:48:00"), presenter: "AV", location: "Center & Sidescreens", avMedia: "Audio", audioSource: "Resolume", sideScreens: "Bang",
+          centerScreen: "Bang", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"},
+          {cueNumber: 4, title: "Manglacharan", startTime: new Date("2024-12-21T17:48:00"), endTime: new Date("2024-12-21T17:51:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
+            centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"},
+            {cueNumber: 5, title: "Welcome Emcee", startTime: new Date("2024-12-21T17:51:00"), endTime: new Date("2024-12-21T17:53:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
+              centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"},
+              {cueNumber: 2, title: "Countdown", startTime: new Date("2024-12-21T17:40:00"), endTime: new Date("2024-12-21T17:45:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
+                centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"}, 
+                {cueNumber: 3, title: "Bang Video", startTime: new Date("2024-12-21T17:45:00"), endTime: new Date("2024-12-21T17:48:00"), presenter: "AV", location: "Center & Sidescreens", avMedia: "Audio", audioSource: "Resolume", sideScreens: "Bang",
+                  centerScreen: "Bang", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"},
+                  {cueNumber: 4, title: "Manglacharan", startTime: new Date("2024-12-21T17:48:00"), endTime: new Date("2024-12-21T17:51:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
+                    centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"},
+                    {cueNumber: 5, title: "Welcome Emcee", startTime: new Date("2024-12-21T17:51:00"), endTime: new Date("2024-12-21T17:53:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
+                      centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"}
+  ]
+
+  let cuesLength: number = cues.length - 1;
+
+  const project1: Project = {
+    title: "DE MMXXIV", date: new Date(2024, 11, 21), startTime: new Date("2024-12-21T17:25:00"), endTime: new Date("2024-12-21T20:16:00"), duration: new Date(0,0,0,2,51),
+    cues: cues, cueAmount: 27
+  }
   return (
-    <div>
-        <h1>CueInput</h1>
+    <>
+      <AppHeader />
+      <Container fluid className="CueInput-body d-flex align-items-center justify-content-center">
+      <div className="scroll-container-cueInput">
+  <div className="scroll-content-cueInput">
+    {cues.map((cue) => (
+      <Card key={cue.cueNumber} className="CueInput-Cue">
+        <Card.Body>
+          <Row>
+            <Col xs={3} className='cueNumber'>
+              <h5 className="inter-bold" style={{ margin: 0 }}>{cue.cueNumber}</h5>
+            </Col>
+            <Col>
+              <h5 className="inter-bold" style={{ margin: 4 }}> {cue.title}</h5>
+            </Col>
+          </Row>
+          <hr style={{ borderTop: '3px solid #578493', borderRadius: '10px', minWidth: '325px', marginTop: 10, marginBottom: 0, borderStyle: "solid", opacity: '1'}} />
+
+          <Row className="align-items-center">
+            <Col xs={5}>
+              <p className='inter-medium' style={{ margin: 10 }}>Start: {cue.startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
+            </Col>
+            <Col xs={2} className="d-flex justify-content-center" style={{ marginLeft: 11 }}>
+              <div className="vertical-line"></div>
+            </Col>
+            <Col xs={1} style={{ paddingLeft: 0 }}>
+              <p className='inter-medium' style={{ margin: 10 }}>End: {cue.endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
+            </Col>
+          </Row>
+          <hr style={{ borderTop: '3px solid #578493', borderRadius: '10px', minWidth: '325px', marginTop: 0, borderStyle: "solid", opacity: '1'}} />
+
+          <Row className='section'>
+            <p className='inter-medium' style={{ margin: 5 }}>Presenter: <span className='inter-bold'> {cue.presenter}</span></p>
+          </Row>
+          <Row className='section'>
+            <p className='inter-medium' style={{ margin: 5 }}>Location: <span className='inter-bold'> {cue.location}</span></p>
+          </Row>
+          <Row className='section'>
+            <p className='inter-medium' style={{ margin: 5 }}>AV Media/Audio: <span className='inter-bold'> {cue.avMedia}</span></p>
+          </Row>
+          <Row className='section'>
+            <p className='inter-medium' style={{ margin: 5 }}>Audio Source: <span className='inter-bold'> {cue.audioSource}</span></p>
+          </Row>
+          <Row className='section'>
+            <p className='inter-medium' style={{ margin: 5 }}>Side Screens: <span className='inter-bold' style={{ margin: 0 }}> {cue.sideScreens}</span></p>
+          </Row>
+          <Row className='section'>
+            <p className='inter-medium' style={{ margin: 5 }}>Center Screen: <span className='inter-bold'> {cue.centerScreen}</span></p>
+          </Row>
+          <Row className='section'>
+            <p className='inter-medium' style={{ margin: 5 }}>Lighting: <span className='inter-bold'> {cue.lighting}</span></p>
+          </Row>
+          <Row className='section'>
+            <p className='inter-medium' style={{ margin: 5 }}>Ambient Lights: <span className='inter-bold'> {cue.ambientLights}</span></p>
+          </Row>
+          <Row className="section d-flex justify-content-start w-100">
+            <Col xs="auto" className="p-1">
+              <p className="inter-medium" style={{ marginLeft: 14 }}>Notes:</p>
+            </Col>
+            <Col className="p-1">
+              <p className="inter-bold text-wrap" style={{ margin: 0 }}>{cue.notes}</p>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    ))}
     </div>
+    </div>
+        <Row className="justify-content-center text-center mt-3 mb-4" style={{padding: '2%'}}>
+          <Col>
+            <img src={addMoreButton} height="70px" alt="Add More" />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
--- src/Pages/Home Page/HomePage.tsx
+++ src/Pages/Home Page/HomePage.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './HomePage.css';
 import { AppHeader } from '../../Components/Header/Header';
-import {Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import addMoreButton from '../../Assets/Home-Page/Add-More-Button.png';
 import editButton from '../../Assets/Home-Page/Edit-Button.png';
 import liveButton from '../../Assets/Home-Page/Live-Button.png';
 import { Project } from '../../Interfaces/Project/Project';
 import { Cue } from '../../Interfaces/Cue/Cue';
-
 
 function HomePage() {
   const navigate = useNavigate();
@@ -18,72 +17,89 @@ function HomePage() {
       centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Purple", notes: "Slow movements"}, 
       {cueNumber: 2, title: "Countdown", startTime: new Date("2024-12-21T17:40:00"), endTime: new Date("2024-12-21T17:45:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
         centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"}, 
-        {cueNumber: 3, title: "Bang Video", startTime: new Date("2024-12-21T17:40:00"), endTime: new Date("2024-12-21T17:45:00"), presenter: "AV", location: "Center & Sidescreens", avMedia: "Audio", audioSource: "Resolume", sideScreens: "Bang",
+        {cueNumber: 3, title: "Bang Video", startTime: new Date("2024-12-21T17:45:00"), endTime: new Date("2024-12-21T17:48:00"), presenter: "AV", location: "Center & Sidescreens", avMedia: "Audio", audioSource: "Resolume", sideScreens: "Bang",
           centerScreen: "Bang", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"},
-          {cueNumber: 4, title: "Manglacharan", startTime: new Date("2024-12-21T17:40:00"), endTime: new Date("2024-12-21T17:45:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
+          {cueNumber: 4, title: "Manglacharan", startTime: new Date("2024-12-21T17:48:00"), endTime: new Date("2024-12-21T17:51:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
             centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"},
-            {cueNumber: 5, title: "Welcome Emcee", startTime: new Date("2024-12-21T17:40:00"), endTime: new Date("2024-12-21T17:45:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
+            {cueNumber: 5, title: "Welcome Emcee", startTime: new Date("2024-12-21T17:51:00"), endTime: new Date("2024-12-21T17:53:00"), presenter: "AV", location: "Sidescreens", avMedia: "Audio", audioSource: "ProPres", sideScreens: "Countdown Vid",
               centerScreen: "Blackout", lighting: "Gobos", ambientLights: "Blue", notes: "Slow movements"}
   ]
 
   let cuesLength: number = cues.length - 1;
 
   const project1: Project = {
-    title: "DE MMXXIV", date: new Date("2024-12-21"), startTime: new Date("2024-12-21T17:25:00"), endTime: new Date("2024-12-21T20:16:00"), duration: new Date(0,0,0,2,51),
+    title: "DE MMXXIV", date: new Date(2024, 11, 21), startTime: new Date("2024-12-21T17:25:00"), endTime: new Date("2024-12-21T20:16:00"), duration: new Date(0,0,0,2,51),
     cues: cues, cueAmount: 27
   }
-  
-  
   return (
     <>
-    <AppHeader/>
-    <Container fluid className="HomePage-body d-flex align-items-center justify-content-center">
-    <Row className="justify-content-center text-center mt-3 mb-4">
+      <AppHeader />
+      <Container fluid className="HomePage-body d-flex align-items-center justify-content-center">
+        <Row className="justify-content-center text-center mt-3 mb-4" style={{padding: '2%'}}>
           <Col>
-            <img src={addMoreButton} height="70px" alt="Add More" style={{paddingLeft: '15%'}} />
+            <img src={addMoreButton} height="70px" alt="Add More" />
           </Col>
         </Row>
-        <Row className="w-100 justify-content-center">
-          <Col xs={10} sm={8} md={6} lg={4} className="d-flex justify-content-center">
-            <Card className="HomePage-Project1">
+        <div className="scroll-container">
+          <div className="scroll-content">
+          <Card className="HomePage-Project1">
               <Card.Body>
-                <h1 className="inter-extrabold">{project1.title.toString()}</h1>
-                <h3 className="inter-bold" style={{paddingBottom: '5%'}}>December 21, 2024</h3>
-                <h3 className="inter-bold">Details</h3>
-                <p className="inter-semibold" style={{marginBottom: '1%'}}>
-  Start Time: {cues[0].startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}
-</p>
-<p className="inter-semibold" style={{marginBottom: '1%'}}>
-  End Time: {cues[cuesLength].endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}
-</p>
-<p className="inter-semibold" style={{ paddingBottom: '5%', marginBottom:'1%'}} >
-  Duration: {Math.floor((cues[cuesLength].endTime.getTime() - cues[0].startTime.getTime()) / 60000)} minutes
-</p>
-                <h3 className="inter-bold">Cues:</h3>
-                <p className="inter-semibold" style={{marginBottom: '1%'}}>01: {cues[0].title}</p>
-                <p className="inter-semibold" style={{marginBottom: '1%'}}>02: {cues[1].title}</p>
-                <p className="inter-semibold" style={{marginBottom: '1%'}}>03: {cues[2].title}</p>
-                <p className="inter-semibold" style={{marginBottom: '1%'}}>04: {cues[3].title}</p>
-                <p className="inter-semibold" style={{marginBottom: '1%', paddingBottom: '5%'}}>05: {cues[4].title}</p>
+                <h1 className="inter-bold">{project1.title.toString()}</h1>
+                <h3 className="inter-semibold" style={{paddingBottom: '5%'}}>{project1.date.toLocaleDateString([], {month: 'long', day: 'numeric', year: 'numeric'})}</h3>
+                <h3 className="inter-semibold">Details</h3>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>
+                  Start Time: {cues[0].startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}
+                </p>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>
+                  End Time: {cues[cuesLength].endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}
+                </p>
+                <p className="inter-medium" style={{ paddingBottom: '5%', marginBottom:'1%'}} >
+                  Duration: {Math.floor((cues[cuesLength].endTime.getTime() - cues[0].startTime.getTime()) / 60000)} minutes
+                </p>
+                <h3 className="inter-semibold">Cues: {project1.cueAmount}</h3>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>01: {cues[0].title}</p>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>02: {cues[1].title}</p>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>03: {cues[2].title}</p>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>04: {cues[3].title}</p>
+                <p className="inter-medium" style={{marginBottom: '1%', paddingBottom: '5%'}}>05: {cues[4].title}</p>
                 <div className="d-flex justify-content-between">
                   <img src={editButton} height="40px" alt="Edit" onClick={() => navigate("/CueInput")} style={{paddingLeft: '15%'}}/>
                   <img src={liveButton} height="40px" alt="Live" onClick={() => navigate('/LiveCueSheet')} style={{paddingRight: '20%'}} />
                 </div>
               </Card.Body>
             </Card>
-          </Col>
-
-          <Col xs={10} sm={8} md={6} lg={4} className="d-flex justify-content-center">
             <Card className="HomePage-Project1">
               <Card.Body>
-                <h1>Project 2</h1>
+              <h1 className="inter-bold">Untitled</h1>
+                <h3 className="inter-semibold" style={{paddingBottom: '5%'}}>Date</h3>
+                <h3 className="inter-semibold">Details</h3>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>
+                  Start Time:
+                </p>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>
+                  End Time:
+                </p>
+                <p className="inter-medium" style={{ paddingBottom: '5%', marginBottom:'1%'}} >
+                  Duration: 
+                </p>
+                <h3 className="inter-semibold">Cues: </h3>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>01: </p>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>02: </p>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>03: </p>
+                <p className="inter-medium" style={{marginBottom: '1%'}}>04: </p>
+                <p className="inter-medium" style={{marginBottom: '1%', paddingBottom: '5%'}}>05: </p>
+                <div className="d-flex justify-content-between">
+                  <img src={editButton} height="40px" alt="Edit" onClick={() => navigate("/CueInput")} style={{paddingLeft: '15%'}}/>
+                  <img src={liveButton} height="40px" alt="Live" onClick={() => navigate('/LiveCueSheet')} style={{paddingRight: '20%'}} />
+                </div>
               </Card.Body>
             </Card>
-          </Col>
-        </Row>
+            
+          </div>
+        </div>
       </Container>
     </>
-)
+  );
 }
 
 export default HomePage;
