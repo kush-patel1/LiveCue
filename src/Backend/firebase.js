3b9a5996a94565f1e76413763ca20efb4838686c
Kush Patel <kushptl@udel.edu>
2025-03-15 00:47:32 -0400

    test to see if this works. got ride of browserouter

--- src/Backend/firebase.js
+++ src/Backend/firebase.js
@@ -8,7 +8,7 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'AIzaSyDIXIql505q9RQBJiOQ5DiuwZvwVG5VFSg',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
